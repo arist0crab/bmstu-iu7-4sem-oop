e4db9370fa369a7fe097d7d80091facec865dcc1
Varvara Bondareva <BondareVarechka@yandex.ru>
2026-05-07 13:11:16 +0300

    DrawFactorySolution has been added

---
+++ lab_03/DrawManager/DrawFactory/DrawFactorySolution.cpp
@@ -0,0 +1,9 @@
+#include "DrawFactorySolution.hpp"
+
+
+template <typename TFactory, typename TScene>
+std::unique_ptr<BaseDrawerCreator> DrawFactorySolution::createDrawer(std::shared_ptr<TScene> scene)
+{
+    std::shared_ptr<BaseDrawerCreator> creator = std::make_shared<TFactory>(scene);
+    return creator->createDrawer();
+}   
