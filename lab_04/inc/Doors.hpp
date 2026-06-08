015fa3aeada4f9c1104375524061e82961aca44b
Varvara Bondareva <BondareVarechka@yandex.ru>
2026-06-08 14:28:24 +0300

    fiirst version of Doors has been done

---
+++ lab_04/inc/Doors.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "data.hpp"
+#include <QObject>
+#include <QTimer>
+
+
+enum class DoorsState
+{
+    OPEN,
+    CLOSE,
+    OPENING,
+    CLOSING
+};
+
+
+class Doors : public QObject
+{
+    public:
+        explicit Doors(QObject* parent = nullptr);
+        ~Doors() override = default;
+
+        DoorsState getState() const noexcept;
+
+    signals:
+        void openedSignal();
+        void closedSignal();
+        void doorsStateChanged(DoorsState state);
+
+    public slots:
+        void startOpeningSlot(); 
+        void openSlot();
+        void startClosingSlot();
+        void closeSlot();
+
+    private:
+        DoorsState _state;
+        QTimer _timer;
+};
---
+++ lab_04/src/Doors.cpp
@@ -0,0 +1,75 @@
+#include "Doors.hpp"
+
+
+Doors::Doors(QObject* parent = nullptr) : _state(DoorsState::CLOSE), _timer(this) 
+{ 
+    _timer.setSingleShot(true);
+
+    connect(&_timer, &QTimer::timeout, this, [this]() 
+    {
+        if (_state == DoorsState::OPENING)
+            emit openedSignal();
+        else if (_state == DoorsState::CLOSING)
+            emit closedSignal();
+    });
+}
+
+
+DoorsState Doors::getState() const noexcept
+{
+    return _state;
+}
+
+
+// =========================
+//           Слоты
+// =========================
+
+
+void Doors::startOpeningSlot()
+{
+    if (_state != DoorsState::CLOSE && _state != DoorsState::CLOSING)
+        return;
+
+    _state = DoorsState::OPENING;
+    emit doorsStateChanged(_state);
+
+    _timer.start(DOOR_TIME);
+}
+
+
+void Doors::openSlot()
+{
+    if (_state != DoorsState::OPENING)
+        return;
+
+    _state = DoorsState::OPEN;
+    emit doorsStateChanged(_state);
+
+    _timer.start(DOOR_TIME);
+}
+
+
+void Doors::startClosingSlot()
+{
+    if (_state != DoorsState::OPEN)
+        return;
+
+    _state = DoorsState::CLOSING;
+    emit doorsStateChanged(_state);
+
+    _timer.start(DOOR_TIME);
+}
+
+
+void Doors::closeSlot()
+{
+    if (_state != DoorsState::CLOSING)
+        return;
+
+    _state = DoorsState::CLOSE;
+    emit doorsStateChanged(_state);
+
+    _timer.start(DOOR_TIME);
+}
+
